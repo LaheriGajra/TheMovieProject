6d5f0958cba3b9d107c11f8ed385e1e045bb68e3
Nikhil Patel <86871570+Nikhilpatel22@users.noreply.github.com>
2022-06-08 10:58:34 +0530

    add
    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Moviehome from './movie/Moviehome';
+import MovieDetails from './movie/MovieDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>     
+          <Route path='/' element={<Moviehome/>}></Route>
+         
+          <Route path='/moviedetails/:movieId' element={<MovieDetails/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
